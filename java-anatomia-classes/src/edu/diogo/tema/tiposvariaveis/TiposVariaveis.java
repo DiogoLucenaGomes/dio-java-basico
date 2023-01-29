d8b2be8870083dcac3be24d4014e5c3b734ba25a
Diogo Lucena Gomes <diogo.lucenape@gmail.com>
2023-01-29 16:16:03 -0300

    aula tipo de variáveis

---
+++ java-anatomia-classes/src/edu/diogo/tema/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package edu.diogo.tema.tiposvariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salario = 2500;
+
+        short numerotoCurto = 1;
+        int numeroNormal = numerotoCurto;
+        short mumeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+
+}
